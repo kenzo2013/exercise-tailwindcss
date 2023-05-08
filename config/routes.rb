ee438ab545ae6cf9d8a1d24dba9f5294a0bcc81a
Simon Eric Noumedem <kkenzo2007@yahoo.fr>
2023-05-08 17:51:28 +0200

    New letter

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'cta/index'
-  get 'content_sections/index'
-
   root "homes#index"
 
   resources :blogs, only: :index
   resources :content_sections, only: :index
   resources :cta, only: :index
   resources :ecommercelist, only: :index
+  resources :newsletters, only: :index
 end
